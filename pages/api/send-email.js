527985ace72c8f236ed817aceac7abeb934c9426
enes.kuccuk <enes.kuccuk@solvoyo.com>
2024-08-07 00:31:48 +0300

    chore: Update nodemailer configuration for sending emails
    Refactor the nodemailer configuration in the send-email API endpoint to use the correct host, port, and authentication credentials. This ensures that emails are sent successfully from the application.
    
    Co-authored-by: [Author Name]

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -1,51 +1,28 @@
 import nodemailer from "nodemailer";
 
-const template = ({ name,surname, phone, email, message }) => {
-  return `<table>
-  <tr>
-    <td><strong>Ad Soyad:</strong></td>
-    <td>${name} ${surname}</td>
-  </tr>
-  <tr>
-    <td><strong>Telefon:</strong></td>
-    <td>${phone}</td>
-  </tr>
-  <tr>
-    <td><strong>E-posta:</strong></td>
-    <td>${email}</td>
-  </tr>
-  <tr>
-    <td><strong>Mesaj:</strong></td>
-    <td>${message}</td>
-  </tr>
-</table>`;
-};
-
-// create reusable transporter object using the default SMTP transport
-const transporter = nodemailer.createTransport({
-  host: "mail.victoriatransfers.com",
-  port: 465,
-  secure: false,
-  auth: {
-    user: "noreply@victoriatransfers.com", // generated ethereal user
-    pass: "=!q^C-(52;6(", // generated ethereal password
-  },
-  tls: {
-    // do not fail on invalid certs
-    rejectUnauthorized: false,
-  },
-});
-
 export default async function handler(req, res) {
   try {
-    const info = await transporter.sendMail({
-      from: "noreply@victoriatransfers.com", // sender address
-      to: "victoriatransfers@gmail.com,info@victoriatransfers.com", // list of receivers
-      subject: "iletisim formu", // Subject line
-      html: template(req.body), // html body
+    const transporter = nodemailer.createTransport({
+      host: "mail.victoriatransfers.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "victoriatransfer",
+        pass: "x7eLq_G[X37?",
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
     });
-
-    res.status(200).json({ status: "OK", messageId: info.messageId });
+    
+    transporter.verify(function (error, success) {
+      if (error) {
+        res.status(500).json({ status: "ERROR", message: e.message });
+      } else {
+        res.status(200).json({ status: "OK", messageId: info.messageId });
+      }
+    });
+  
   } catch (e) {
     res.status(500).json({ status: "ERROR", message: e.message });
   }
